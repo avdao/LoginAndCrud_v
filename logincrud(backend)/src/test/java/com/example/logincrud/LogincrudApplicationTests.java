74800fe324d678a1fa5179213504da00ad189ee8
avdao <76061468+avdao@users.noreply.github.com>
2022-05-08 22:35:10 +0200

    Add files via upload

---
+++ logincrud(backend)/src/test/java/com/example/logincrud/LogincrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logincrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogincrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
